70c0e3c229bff2d34b4404606856b4552b8e2692
teargle <teargle@163.com>
2019-12-22 16:43:34 +0800

    邮件发送
    邮件发送

--- application/index/controller/Api.php
+++ application/index/controller/Api.php
@@ -4,6 +4,10 @@ namespace app\index\controller;
 use \think\Controller;
 use \think\View;
 use \think\Request;
+use PHPMailer\PHPMailer;
+use PHPMailer\Exception;
+use PHPMailer\SMTP;
+use app\index\model\Dict;
 
 use app\index\model\Contact;
 
@@ -28,6 +32,44 @@ class Api extends Controller
         $cantact = new Contact ;
         $cantact->data($data) ;
         $cantact->save() ;
+
+        $content = "称呼： " . $post ['name'] . "<br />";
+        $content .= "联系方式: " . $post ['contact'] . "<br />";
+        $content .= "信息内容： " . $post ['information'] . "<br />";
+        $this->_send_email ( $content );
+
         return output_json ( true , "OK" , null ) ;
 	}
+
+    private function _send_email( $content ) {
+        $dict = Dict::where('model' , 'email')->select();
+        $dict = array_column($dict, 'value' , 'name');
+        $mail = new PHPMailer(true);
+        try {
+            //Server settings
+            $mail->CharSet = "UTF-8";
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP( true );                                            // Send using SMTP
+            $mail->Host       = $dict ['smtp'] ;//                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = $dict ['address'] ;                    // SMTP username
+            $mail->Password   = $dict ['secure'] ;                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;      
+            // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = $dict ['port'] ;                      // TCP port to connect to
+            $mail->SMTPDebug = 0;
+            //Recipients
+            $mail->setFrom($dict ['address'], 'DN');
+            $mail->addAddress($dict ['address'], 'DN');     // Add a recipient
+
+            // Content
+            $mail->Subject = 'WEBSITE INFORMATION';
+            //$mail->Body    = $content;
+            $mail->msgHTML($content);
+
+            $mail->send();
+        } catch (Exception $e) {
+            
+        }
+    }
 }
--- application/index/controller/Utils.php
+++ application/index/controller/Utils.php
@@ -4,6 +4,9 @@ namespace app\index\controller ;
 
 use \think\Controller;
 use think\Request;
+use PHPMailer\PHPMailer;
+use PHPMailer\Exception;
+use PHPMailer\SMTP;
 
 /**
  * 
@@ -28,4 +31,40 @@ class Utils extends Controller
 		echo url('index/index/comment') ;
 		exit ;
 	}
+
+	function tb () {
+		$mail = new PHPMailer(true);
+		try {
+		    //Server settings
+		    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+		    $mail->isSMTP();                                            // Send using SMTP
+		    $mail->Host       = 'smtp.163.com';                    // Set the SMTP server to send through
+		    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+		    $mail->Username   = 'teargle@163.com';                     // SMTP username
+		    $mail->Password   = 'teargle830906';                               // SMTP password
+		    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;      
+    		// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+		    $mail->Port       = 465;                                    // TCP port to connect to
+
+		    //Recipients
+		    $mail->setFrom('teargle@163.com', 'Mailer');
+		    $mail->addAddress('teargle@163.com', 'lei.xiong');     // Add a recipient
+
+		    // Content
+		    // $mail->isHTML(true);                                  // Set email format to HTML
+		    $mail->Subject = 'Here is the subject';
+		    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+		    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		    $mail->send();
+		    echo 'Message has been sent';
+		} catch (Exception $e) {
+		    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+		}
+	}
+
+	public function tc () {
+		phpinfo() ;
+	}
+
 }
